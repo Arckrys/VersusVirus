e6dda0663e45a1c2bb468668f836f9b13429cf36
Arckrys <aymeric.damour06@gmail.com>
2020-04-03 23:00:37 +0200

    2ndPage

--- app/src/main/java/com/team20/versusvirus/MainActivity.java
+++ app/src/main/java/com/team20/versusvirus/MainActivity.java
@@ -2,7 +2,11 @@ package com.team20.versusvirus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("j'ai modifié mon texte");
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
