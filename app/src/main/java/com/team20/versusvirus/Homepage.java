e7d615bd0b6daef242dcf052700e8414e558ac7f
Arckrys <aymeric.damour06@gmail.com>
2020-04-04 04:43:47 +0200

    avancement du front end part 2

--- app/src/main/java/com/team20/versusvirus/Homepage.java
+++ app/src/main/java/com/team20/versusvirus/Homepage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Homepage extends AppCompatActivity {
 
@@ -13,11 +14,35 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
-        ImageView imageView = findViewById(R.id.nextRecipeImage);
-        imageView.setOnClickListener(new View.OnClickListener() {
+        ImageView nextRecipeImage = findViewById(R.id.nextRecipeImage);
+        nextRecipeImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Homepage.this, RecipePage.class);
+                startActivity(intent);
+            }
+        });
+        ImageView profilePicture = findViewById(R.id.profileImage);
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Homepage.this, ProfilPage.class);
+                startActivity(intent);
+            }
+        });
+        TextView profilelink = findViewById(R.id.myProfileTextView);
+        profilelink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Homepage.this, ProfilPage.class);
+                startActivity(intent);
+            }
+        });
+        TextView profileName = findViewById(R.id.myNameTextView);
+        profileName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Homepage.this, ProfilPage.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/team20/versusvirus/ProfilPage.java
@@ -0,0 +1,33 @@
+package com.team20.versusvirus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ProfilPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_page);
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilPage.this, Homepage.class);
+                startActivity(intent);
+            }
+        });
+        Button disconnectButton = findViewById(R.id.disconnectButton);
+        disconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilPage.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
