fe2b6ef9facb1dd9a2f88b590785aa5cb3e20aef
matthiaszeller <44340346+maousi@users.noreply.github.com>
2020-04-04 12:09:56 +0200

    Firebase database access ok
    Load a file from the database and change the image of the recipe

--- app/src/main/java/com/team20/versusvirus/RecipePage.java
+++ app/src/main/java/com/team20/versusvirus/RecipePage.java
@@ -1,11 +1,20 @@
 package com.team20.versusvirus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class RecipePage extends AppCompatActivity {
 
@@ -21,5 +30,45 @@ public class RecipePage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        testStorage();
+    }
+
+    private void testStorage() {
+        // Create the storage
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        // https://firebase.google.com/docs/storage/android/create-reference
+        // Create the reference singleton instance
+        StorageReference storageRef = storage.getReference();
+
+        // Create reference to recipe images
+        // recipe images are in the folder /img/recipe/
+        StorageReference recipeRef = storageRef.child("img/recipe");
+
+        // Reference to specific image
+        StorageReference chickenRef = recipeRef.child("chicken-cacciatore-one-pot.jpg");
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+
+        chickenRef.getBytes(2 * ONE_MEGABYTE).addOnSuccessListener(
+                new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Bitmap img = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        ImageView imgView = findViewById(R.id.imageView);
+                        imgView.setImageBitmap(img);
+                    }
+                }
+        ).addOnFailureListener(
+                new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("ERROR LOADING THE IMG");
+                    }
+                }
+        );
+
     }
 }
