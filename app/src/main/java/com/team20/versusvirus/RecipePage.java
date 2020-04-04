80540a3968a28187edfac505033ca12194dc6edb
Juliette <juliette.challot@epfl.ch>
2020-04-04 23:57:24 +0200

    petites toques toutes mimi

--- app/src/main/java/com/team20/versusvirus/RecipePage.java
+++ app/src/main/java/com/team20/versusvirus/RecipePage.java
@@ -10,14 +10,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 public class RecipePage extends AppCompatActivity {
-
+    RatingBar ratingBar1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +30,14 @@ public class RecipePage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        /*ratingBar1 = findViewById(R.id.ratingBar1);
+        ratingBar1.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+            Toast.makeText(RecipePage.this, "Difficulty: " + (int)rating, Toast.LENGTH_SHORT).show();
+            }
+            });*/
+
 
 
         //testStorage();
