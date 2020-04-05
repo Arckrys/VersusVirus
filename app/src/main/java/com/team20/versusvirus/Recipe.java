78e899411e04b0667ad35e8def1f7b9de2343d2e
Arckrys <aymeric.damour06@gmail.com>
2020-04-05 13:58:03 +0200

    Update Recipe.java

--- app/src/main/java/com/team20/versusvirus/Recipe.java
+++ app/src/main/java/com/team20/versusvirus/Recipe.java
@@ -4,17 +4,18 @@ import java.security.Timestamp;
 import java.util.List;
 
 public class  Recipe{
-    public Integer id_Recipe,prepTim,cookTime,difficulty;
+    public Integer id_Recipe,prepTim,cookTime,difficulty,numberParticipent;
     public String image,title, language,description;
     public Timestamp date;
     public List<Ingredient> ingredients;
     public List<String> steps;
 
-    public Recipe(Integer id_Recipe, Integer prepTim, Integer cookTime, Integer difficulty, String image, String title, String language, String description, Timestamp date) {
+    public Recipe(Integer id_Recipe, Integer prepTim, Integer cookTime, Integer difficulty, Integer numberParticipent, String image, String title, String language, String description, Timestamp date) {
         this.id_Recipe = id_Recipe;
         this.prepTim = prepTim;
         this.cookTime = cookTime;
         this.difficulty = difficulty;
+        this.numberParticipent = numberParticipent;
         this.image = image;
         this.title = title;
         this.language = language;
